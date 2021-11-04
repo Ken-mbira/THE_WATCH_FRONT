adaa21ff3d559241b9a502a4ebb228c5e41fda93
ken <ken.mbira@student.moringaschool.com>
2021-11-04 21:00:57 +0300

    link image field to cloudinary

--- src/app/hood/hood.component.ts
+++ src/app/hood/hood.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AccountService } from '../account.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-hood',
@@ -9,6 +10,7 @@ import { AccountService } from '../account.service';
 })
 export class HoodComponent implements OnInit {
   hood:any
+  cloudinaryUrl = environment.CLOUDINARY_URL
 
   constructor(private accountService:AccountService) { }
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  CLOUDINARY_URL:'https://res.cloudinary.com/duykqvfcr/'
 };
